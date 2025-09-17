9ae4f406d7d9ec2714cae0f92fcbda3042fb7d7e
Mike Centola <mcentola@appliedengdesign.com>
2025-09-16 20:05:23 -0400

    chore: finalize eslint config update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,70 +14,102 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-
-    globalIgnores([
-        'out/*',
-        'dist/*',
-    ]),
+    globalIgnores(['out/*', 'dist/*']),
 
     {
-        files: [
-            'src/**/*.ts',
-            'scripts/**/*.ts',
-            'test/**/*.ts'
-        ],
+        files: ['src/**/*.ts', 'scripts/**/*.ts', 'test/**/*.ts'],
         ...js.configs.recommended,
-        ...tseslint.configs.recommendedTypeChecked[0],
 
         languageOptions: {
             sourceType: 'module',
             parser: tseslint.parser,
             parserOptions: {
                 ecmaVersion: 2024,
+                'sourceType': 'module',
                 ecmaFeatures: {
-                    impliedStrict: true
+                    impliedStrict: true,
                 },
                 projectService: true,
                 tsconfigRootDir: import.meta.dirname,
             },
             globals: {
                 ...globals.node,
-                ...globals.mocha
-            }
+                ...globals.mocha,
+            },
         },
 
-        plugins:{
-            'import-x': importX, 
-            '@typescript-eslint': tseslint.plugin
+        plugins: {
+            'import-x': importX,
+            '@typescript-eslint': tseslint.plugin,
         },
 
-        extends: [ 'import-x/flat/recommended', 'import-x/flat/typescript'],
+        extends: [
+            '@typescript-eslint/recommended-type-checked',
+            'import-x/flat/recommended',
+            'import-x/flat/typescript',
+        ],
 
         rules: {
             '@typescript-eslint/naming-convention': [
-				'warn',
-				{
-					'selector': 'class',
-					'format': [
-						'PascalCase'
-					],
-					'leadingUnderscore': 'allow'
-				}
-			],
-            '@typescript-eslint/no-floating-promises': 'error',
+                'warn',
+                {
+                    'selector': 'class',
+                    'format': ['PascalCase'],
+                    'leadingUnderscore': 'allow',
+                },
+            ],
+            '@typescript-eslint/no-empty-function': [
+                'warn',
+                {
+                    'allow': ['constructors'],
+                },
+            ],
+            '@typescript-eslint/no-inferrable-types': [
+                'warn',
+                {
+                    'ignoreParameters': true,
+                    'ignoreProperties': true,
+                },
+            ],
             '@typescript-eslint/no-unused-expressions': [
-                'error', 
+                'error',
                 {
                     'allowShortCircuit': true,
-                    'allowTernary': true, 
-                }
+                    'allowTernary': true,
+                },
             ],
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    'args': 'all',
+                    'argsIgnorePattern': '^_',
+                    'caughtErrors': 'all',
+                    'caughtErrorsIgnorePattern': '^_',
+                    'destructuredArrayIgnorePattern': '^_',
+                    'varsIgnorePattern': '^_',
+                    'ignoreRestSiblings': true,
+                },
+            ],
+            '@typescript-eslint/no-use-before-define': 'error',
             'curly': ['error', 'all'],
+            'dot-notation': 'error',
             'eqeqeq': 'error',
+            'import-x/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+            'import-x/extensions': [
+                'error',
+                'ignorePackages',
+                {
+                    'fix': true,
+                },
+            ],
+            'import-x/newline-after-import': 'error',
+            'import-x/no-absolute-path': 'error',
+            'import-x/no-cycle': 'off',
             'import-x/no-dynamic-require': 'error',
             'import-x/no-default-export': 'error',
             'import-x/no-self-import': 'error',
-            'import-x/order': [ 'error',
+            'import-x/order': [
+                'error',
                 {
                     'alphabetize': {
                         'order': 'asc',
@@ -86,35 +118,39 @@ export default defineConfig([
                     },
                     'named': true,
                     'sortTypesGroup': true,
-                }
+                },
             ],
-            'max-len': ['error', {
-                'code': 120, // needs to be in sync w/ .prettierrc printWidth
-                'ignoreUrls': true,
-            }],
+            'max-classes-per-file': ['error', 1],
+            'no-bitwise': 'error',
+            'no-console': [
+                'error',
+                {
+                    'allow': ['info', 'error'],
+                },
+            ],
             'no-duplicate-imports': 'error',
+            'no-invalid-this': 'error',
             'no-var': 'warn',
-            'prefer-const': 'error',
-            'quotes': ['error', 'single', {
-                'allowTemplateLiterals': true,
-                'avoidEscape': true,
-            }],
+            'prefer-arrow-callback': 'error',
+            'prefer-numeric-literals': 'error',
+            'prefer-object-spread': 'error',
+            'prefer-template': 'error',
+            'radix': 'error',
+
             // Use import-x sort rules instead
             'sort-imports': ['off'],
             'yoda': 'error',
-        }
+        },
     },
 
     {
         // Turn off rules that will cause errors with chai
-        files: [
-            'test/*.ts',
-        ],
+        files: ['test/*.ts'],
 
         rules: {
-           'no-unused-expressions': 'off',
-           '@typescript-eslint/no-unused-expressions': 'off',
-        }
+            'no-unused-expressions': 'off',
+            '@typescript-eslint/no-unused-expressions': 'off',
+        },
     },
 
     // Lint JSON
@@ -124,7 +160,7 @@ export default defineConfig([
         plugins: { json },
         language: 'json/json',
         ...json.configs.recommended,
-        //extends: [ tseslint.configs.disableTypeChecked],   
+        //extends: [ tseslint.configs.disableTypeChecked],
     },
 
     // Lint JSONC
@@ -140,14 +176,14 @@ export default defineConfig([
         files: ['**/*.md'],
         plugins: { markdown },
         language: 'markdown/gfm',
-        extends: [ 'markdown/recommended' ],
+        extends: ['markdown/recommended'],
     },
 
     // Lint YAML
     {
-        files: [ '**/*.yaml', '**/*.yml' ],
-        extends: [ yml.configs['flat/recommended'] ],
+        files: ['**/*.yaml', '**/*.yml'],
+        extends: [yml.configs['flat/recommended']],
     },
 
     esPrettier,
-])
+]);
