e6e324c559602b79cf82a724c8b7329c7cd020d6
Mike Centola <mcentola@appliedengdesign.com>
2025-09-25 15:27:53 -0400

    refactor(helpers): add additional error handling
    Add async try/catch function
    Refactor readDirRecursive function to provide path info and type safety
    
    Add error handling to loadJSON func

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -4,32 +4,132 @@
  * -------------------------------------------------------------------------------------------- */
 
 import { readdir, readFile } from 'node:fs/promises';
-import { basename, dirname, join } from 'node:path';
+import { dirname, join, relative } from 'node:path';
 
+/**
+ * try catch helper function for async error handling
+ *
+ * @param fn - passed arrow function
+ * @param logError - boolean to output error
+ * @returns typed output of error function
+ */
+export const tryCatch = async <T>(
+    fn: () => Promise<T>,
+    logError = false,
+): Promise<[T | undefined, Error | undefined]> => {
+    try {
+        const data = await fn();
+        return [data, undefined];
+    } catch (error: unknown) {
+        const err = error instanceof Error ? error : new Error(String(error));
+
+        if (logError) {
+            console.error(err);
+        }
+
+        return [undefined, err];
+    }
+};
+
+/**
+ * Defines the recursive object for dir parsing
+ */
+interface DirTree {
+    [key: string]: string | string[];
+}
+
+/**
+ * Defines the Options for readDirRecursive
+ */
+interface DirOpts {
+    pattern?: string | RegExp;
+}
+
+/**
+ * Reads a given directory recurisvely and returns a structured object of paths
+ *
+ * @param dirPath - The path to the directory to read
+ * @param opts - Options parameters
+ * @param opts.pattern - A string or RegExp to apply to results
+ * @param result - A DirTree object of the files
+ * @param home - a placeholder for recursive pointer to original dir
+ * @returns
+ */
 export async function readDirRecursive(
     dirPath: string,
-    ignorePattern: string = '',
-    basePath: string = dirPath,
-    result: Record<string, string> = {},
-): Promise<Record<string, string>> {
-    const entries = await readdir(dirPath, { withFileTypes: true });
-    const regex = new RegExp(ignorePattern);
+    opts: DirOpts = { pattern: '.*' },
+    result: DirTree = {},
+    home: string = '',
+): Promise<DirTree> {
+    home = home ? home : dirPath;
 
-    for (const entry of entries) {
-        const fullPath = join(dirPath, entry.name);
-        const dirName = basename(dirname(fullPath));
+    const regex = new RegExp(opts.pattern!);
 
-        if (entry.isDirectory()) {
-            await readDirRecursive(fullPath, ignorePattern, basePath, result);
-        } else if (regex.test(entry.name)) {
-            result[dirName] = fullPath;
+    const [entries, error] = await tryCatch(async () => {
+        return await readdir(dirPath, { withFileTypes: true });
+    });
+
+    if (error) {
+        throw new Error('Error reading directory...', error);
+        return result;
+    }
+    if (entries) {
+        for (const entry of entries) {
+            const fullPath = join(dirPath, entry.name);
+            const keyName = relative(dirname(home), dirPath).replace(/\\/g, '/');
+            if (entry.isDirectory()) {
+                const [res, error] = await tryCatch(async () => {
+                    return await readDirRecursive(fullPath, opts, result, home);
+                });
+                if (error) {
+                    throw new Error('Error reading subdirectory...', error);
+                    continue;
+                } else if (res) {
+                    result = { ...res };
+                }
+            } else {
+                if (!regex.test(entry.name)) {
+                    continue;
+                }
+
+                if (keyName in result) {
+                    if (typeof result[keyName] === 'string') {
+                        result[keyName] = [result[keyName], fullPath];
+                    } else {
+                        result[keyName].push(fullPath);
+                    }
+                }
+                result = { [keyName]: fullPath, ...result };
+            }
         }
     }
-
     return result;
 }
 
+/**
+ * Loads and parses a typed JSON string given a file path
+ *
+ * @typeParam - T the type casting of the JSON object
+ * @param filePath - A string of the path to the file to parse
+ * @param logError - A boolean to display the error or not
+ * @returns a parsed JSON object of type T
+ */
 export async function loadJSON<T>(filePath: string): Promise<T> {
-    const data = await readFile(filePath, 'utf-8');
-    return <T>JSON.parse(data);
+    const [data, err] = await tryCatch(async () => {
+        return await readFile(filePath, 'utf8');
+    });
+
+    if (data) {
+        return new Promise((resolve, reject) => {
+            try {
+                const res = JSON.parse(data) as T;
+                resolve(res);
+            } catch (_err) {
+                reject(new Error('Error parsing JSON file...'));
+            }
+        });
+        //return Promise.resolve(JSON.parse(data) as T);
+    } else {
+        throw new Error(`Error reading file...${err ? err.message : ''}`);
+    }
 }
